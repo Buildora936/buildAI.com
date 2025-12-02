54aa29bb1b6e2f6e4addcfa5f7c38d0ae41f7f8b
BUILDORA <bjosephbrooks07@gmail.com>
2025-12-02 18:08:19 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -16,16 +16,6 @@ if (btn1) btn1.addEventListener("click", () => {
 if (btn2) btn2.addEventListener("click", () => {
     window.location.href = "image-generator.html";
 });
-const btn1 = document.getElementById("openTextTools");
-const btn2 = document.getElementById("cardTextTools");
-
-if (btn1) btn1.addEventListener("click", () => {
-    window.location.href = "Text-tools.html";
-});
-
-if (btn2) btn2.addEventListener("click", () => {
-    window.location.href = "Text-tools.html";
-});
 
 
 // --- DÉCONNEXION FIREBASE ---
